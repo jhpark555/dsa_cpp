be82bb8b4fad24954827545d3cbb6b4a492b1076
Jaehong Park <jhpark555@gmail.com>
2025-02-01 11:43:01 -0800

    update

--- weighted_graph_skienna.cpp
+++ weighted_graph_skienna.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 #define MAXV 100
 #define MAXINT  100000
+#define SET_SIZE 100
 
 int parent[MAXV+1];
 
@@ -21,6 +22,19 @@ typedef struct{
     int nedges;
     int directed;
 }graph;
+
+typedef struct
+{
+    int p[SET_SIZE + 1];    /* parent element */
+    int size[SET_SIZE + 1]; /* number of elements in subtree i */
+    int n;                  /* number of elements in set */
+} union_find;
+
+typedef struct{
+    int x;
+    int y;
+    int weight;
+}edge_pair;
 
 int prim(graph *g, int start){
     int i;
@@ -72,6 +86,99 @@ int prim(graph *g, int start){
 }
 
 
+void union_find_init(union_find *s, int n){
+    int i;
+
+    for(i=1; i<=n ;i++){
+        s->p[i]=i;
+        s->size[i]=1;
+    }
+    s->n =n;
+}
+
+int find(union_find *s, int x){
+    if(s->p[x]==x) return x;   //found
+    return find(s,s->p[x]);   //search recursively with parent
+}
+
+void union_sets(union_find *s, int s1, int s2){
+    int r1, r2;  //root of each sets
+
+    r1=find(s,s1);
+    r2=find(s,s2);
+
+    if(r1==r2)  return;  // already same sets
+
+    if(s->size[r1] >=s->size[r2]) {    // merge to r1 ( r1>r2)
+        s->size[r1]=s->size[r1]+s->size[r2];
+        s->p[r2]=r1;
+    }
+    else{
+        s->size[r2]=s->size[r1]+s->size[r2];
+        s->p[r1]=r2;
+    }
+
+}
+
+bool same_component(union_find *s, int s1, int s2){
+    return (find(s,s1)==find(s,s2));
+}
+
+void to_edge_array(graph *g, edge_pair e[])
+{
+    int i, m;    /* counters */
+    edgenode *p; /* temporary pointer */
+
+    m = 0;
+    for (i = 1; i <= g->nvertices; i++)
+    {
+        p = g->edges[i];
+        while (p != NULL)
+        {
+            if (p->y > i)
+            {
+                e[m].x = i;
+                e[m].y = p->y;
+                e[m].weight = p->weight;
+                m = m + 1;
+            }
+            p = p->next;
+        }
+    }
+}
+
+int weight_compare(const void *e1,const void  *e2)
+{
+  edge_pair *_x= (edge_pair*)e1;
+  edge_pair *_y= (edge_pair*)e2;
+
+  if(_x->weight < _y->weight) return -1;
+  if (_x->weight > _y->weight)  return 1;
+  return 0;
+}
+
+int kruskal(graph *g){
+    int i;
+    union_find s;
+    edge_pair e[MAXV+1];
+    int weight=0;
+
+    union_find_init(&s,g->nvertices);
+
+    to_edge_array(g,e);
+
+    qsort(&e, g->nedges, sizeof(edge_pair), &weight_compare);
+
+    for(i=0;i<(g->nedges) ;i++){
+        if(!same_component(&s,e[i].x, e[i].y)){
+            printf("edge (%d %d) of weight %d in MST\n", e[i].x,e[i].y,e[i].weight);
+            weight = weight + e[i].weight;
+            union_sets(&s, e[i].x, e[i].y);
+        }
+    }
+    return (weight);
+}
+
 void initialize_graph(graph *g, bool directed){
     int i;
     
@@ -120,27 +227,31 @@ void insert_edge(graph *g,int x, int y, int weight, bool directed){
     }
 }
 
+
+
 int main()
 {
     graph g;
     
     initialize_graph(&g,true);
 
-    g.nvertices= 8;
-    insert_edge(&g,1,2,2,0);
-    insert_edge(&g,1,8,4,0);
-    insert_edge(&g,1,7,5,0);
-    insert_edge(&g,2,7,6,0);
+    g.nvertices= 7;
+    insert_edge(&g,1,2,5,0);
+    insert_edge(&g,1,3,12,0);
+    insert_edge(&g,1,4,7,0);
+    insert_edge(&g,2,4,9,0);
     insert_edge(&g,2,5,7,0);
-    insert_edge(&g,2,3,8,0);
-    insert_edge(&g,3,4,9,0);
-    insert_edge(&g,3,5,1,0);
-    insert_edge(&g,4,5,10,0);
-    insert_edge(&g,5,6,11,0);
-    
-    print_graph(&g);
+    insert_edge(&g,3,4,4,0);
+    insert_edge(&g,3,6,7,0);
+    insert_edge(&g,4,5,4,0);
+    insert_edge(&g,4,6,3,0);
+    insert_edge(&g,5,7,5,0);
+    insert_edge(&g,5,6,2,0);
+    insert_edge(&g,6,7,2,0);
 
-    int w= prim(&g,1);
+    // print_graph(&g);
+
+    int w = kruskal(&g); //prim(&g, 1);
     printf("w=%d \n",w);
 
     return (0);
